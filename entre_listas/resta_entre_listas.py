c4cb4b387fd848bf9c1b012585310b49b80e8226
Gael Villarroel <olkersanchez3@gmail.com>
2026-09-07 21:37:35 -0400

    feat(eq02): [HU201] agrega funcion resta entre listas

--- entre_listas/resta_entre_listas.py
+++ entre_listas/resta_entre_listas.py
@@ -1,9 +1,17 @@
 """
 Equipo: Bug Hunters (eq02) | HU: HU-201
-Encargado: [Persona E]
+Encargado: Gael Olker Villarroel Sanchez
 """
 
 def resta_entre_listas(lista1: list, lista2: list) -> list:
     """Resta elemento a elemento entre dos listas del mismo tamano."""
-    # TODO: implementar
-    pass
+
+    if len(lista1) != len(lista2):
+        raise ValueError("Las listas deben tener el mismo tamano.")
+
+    resultado = []
+
+    for i in range(len(lista1)):
+        resultado.append(lista1[i] - lista2[i])
+
+    return resultado
