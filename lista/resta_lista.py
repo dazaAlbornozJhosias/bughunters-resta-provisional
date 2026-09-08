a914c752750b4a5712e9efeb425fdc5ebee83e32
nabo-dev1 <202400310@est.umss.edu>
2026-09-07 20:35:53 -0400

    feat(eq02): [HU201] implementar funcion resta de elementos de lista

--- lista/resta_lista.py
+++ lista/resta_lista.py
@@ -6,4 +6,11 @@ Encargado: [Persona D]
 def resta_lista(numeros: list) -> float:
     """Resta encadenada de todos los numeros de una lista: n1 - n2 - n3 - ..."""
     # TODO: implementar
-    pass
+    if not numeros:
+        raise ValueError("La lista no puede estar vacia.")
+            
+    resultado = numeros[0]
+    for elemento in numeros[1:]:
+        resultado -= elemento
+                
+    return resultado
